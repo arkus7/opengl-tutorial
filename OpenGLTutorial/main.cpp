72d699d2cd1c0a6d633f2923ce1c13ec57ad24e7
Arkadiusz Żmudzin <arkus777@gmail.com>
2016-06-03 23:08:23 +0200

    Show example window

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <GL\glew.h>
+#include "Display.h"
 
-int main() {
-	std::cout << "Hello World";
-
+int main(int argc, char** argv) {
+	Display display(800, 600, "Hello World");
+	while (!display.isClosed())
+	{
+		display.clear(0.0f, 0.15f, 0.3f, 1.0f);
+		display.update();
+	}
 	return 0;
 }
