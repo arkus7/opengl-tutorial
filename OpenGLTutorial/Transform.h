7f6d0f1dd9039cc800036bb696561498f52eef52
Arkadiusz Żmudzin <arkus777@gmail.com>
2016-06-04 21:51:49 +0200

    Fix scaling bug

--- OpenGLTutorial/Transform.h
+++ OpenGLTutorial/Transform.h
@@ -17,7 +17,7 @@ public:
 		glm::mat4 rotationXMatrix = glm::rotate(rotation.x, glm::vec3(1, 0, 0));
 		glm::mat4 rotationYMatrix = glm::rotate(rotation.y, glm::vec3(0, 1, 0));
 		glm::mat4 rotationZMatrix = glm::rotate(rotation.z, glm::vec3(0, 0, 1));
-		glm::mat4 scaleMatrix = glm::translate(scale);
+		glm::mat4 scaleMatrix = glm::scale(scale);
 
 		// matrix multiplying -> backwards
 		glm::mat4 rotationMatrix = rotationZMatrix * rotationYMatrix * rotationXMatrix;
