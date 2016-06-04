c971e3842f526c7207fee2001c4d12d5a2a391fa
Arkadiusz Żmudzin <arkus777@gmail.com>
2016-06-04 20:18:25 +0200

    Add texture, comment filtering (deprecated?)

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -3,6 +3,7 @@
 #include "Display.h"
 #include "Shader.h"
 #include "Mesh.h"
+#include "Texture.h"
 
 int main(int argc, char** argv) {
 	Display display(800, 600, "Hello World");
@@ -16,12 +17,14 @@ int main(int argc, char** argv) {
 	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
 
 	Shader shader("./res/basicShader");
+	Texture texture("./res/bricks.jpg");
 
 	while (!display.isClosed())
 	{
 		display.clear(0.0f, 0.075f, 0.15f, 1.0f);
 		mesh.draw();
 		shader.bind();
+		texture.bind(0);
 		display.update();
 	}
 	return 0;
