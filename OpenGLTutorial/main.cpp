f6ec16a9e15f3d346731ab9f81cb6cf523335297
Arkadiusz Żmudzin <arkus777@gmail.com>
2016-06-04 09:20:33 +0200

    Add  example vertices and draw mesh

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -2,15 +2,25 @@
 #include <GL\glew.h>
 #include "Display.h"
 #include "Shader.h"
+#include "Mesh.h"
 
 int main(int argc, char** argv) {
 	Display display(800, 600, "Hello World");
+
+	Vertex vertices[] = {
+		Vertex(glm::vec3(-0.5f, -0.5f, 0)),
+		Vertex(glm::vec3(0, 0.5, 0)),
+		Vertex(glm::vec3(0.5, -0.5, 0)),
+	};
+
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
 
 	Shader shader("baseShader");
 
 	while (!display.isClosed())
 	{
 		display.clear(0.0f, 0.15f, 0.3f, 1.0f);
+		mesh.draw();
 		shader.bind();
 		display.update();
 	}
