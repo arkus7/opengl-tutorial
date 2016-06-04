fffe3f7a956078aa8c6f71d548aa71e1a6ddbe31
Arkadiusz Żmudzin <arkus777@gmail.com>
2016-06-04 23:45:41 +0200

    Add backface culling

--- OpenGLTutorial/Display.cpp
+++ OpenGLTutorial/Display.cpp
@@ -12,6 +12,7 @@ Display::Display(int width, int height, const std::string& title)
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
 
 
 	window = SDL_CreateWindow(title.c_str(), 
@@ -29,6 +30,11 @@ Display::Display(int width, int height, const std::string& title)
 	}
 
 	closed = false;
+
+	glEnable(GL_DEPTH_TEST);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 }
 
 
@@ -58,5 +64,5 @@ bool Display::isClosed() {
 void Display::clear(float red, float green, float blue, float alpha)
 {
 	glClearColor(red, green, blue, alpha);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
