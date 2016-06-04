bc33bfb1c43c60bdd4e21cdd632e9172aa0f07ac
Arkadiusz Żmudzin <arkus777@gmail.com>
2016-06-04 22:59:26 +0200

    Fix typo in Camera constructor

--- OpenGLTutorial/Camera.h
+++ OpenGLTutorial/Camera.h
@@ -7,12 +7,12 @@ class Camera
 {
 public:
 
-	Camera(const glm::vec3& postition, float fov, float aspect, float zNear, float zFar)
+	Camera(const glm::vec3& position, float fov, float aspect, float zNear, float zFar)
 	{
-		perspectiveMatrix = glm::perspective(fov, aspect, zNear, zFar);
 		this->position = position;
-		forward = glm::vec3(0, 0, 1); // Z axis
-		up = glm::vec3(0, 1, 0); // Y axis
+		this->forward = glm::vec3(0, 0, 1); // Z axis
+		this->up = glm::vec3(0, 1, 0); // Y axis
+		this->perspectiveMatrix = glm::perspective(fov, aspect, zNear, zFar);
 	}
 
 	inline glm::mat4 getViewProjection() const
