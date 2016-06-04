c000a9781bce12857dc26b41a447485ff6ea28c7
Arkadiusz Żmudzin <arkus777@gmail.com>
2016-06-04 23:00:52 +0200

    Add camera to example, add more transforms

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -5,9 +5,13 @@
 #include "Mesh.h"
 #include "Texture.h"
 #include "Transform.h"
+#include "Camera.h"
+
+#define WIDTH 800
+#define HEIGHT 600
 
 int main(int argc, char** argv) {
-	Display display(800, 600, "Hello World");
+	Display display(WIDTH, HEIGHT, "Hello World");
 
 	Vertex vertices[] = {
 		Vertex(glm::vec3(-0.5f, -0.5f, 0), glm::vec2(0.0, 0.0)),
@@ -20,6 +24,7 @@ int main(int argc, char** argv) {
 	Shader shader("./res/basicShader");
 	Texture texture("./res/text.jpg");
 	Transform transform;
+	Camera camera(glm::vec3(0, 0, -2), 70.0f, (float) WIDTH / (float) HEIGHT, 0.1f, 1000.0f);
 
 	float counter = 0.0f;
 
@@ -27,14 +32,18 @@ int main(int argc, char** argv) {
 	{
 		display.clear(0.0f, 0.075f, 0.15f, 1.0f);
 
+		float sinCounter = sinf(counter);
+		float cosCounter = cos(counter);
 
-		transform.getPosition().x = sinf(counter);
+		transform.getPosition().x = sinCounter;
+		transform.getPosition().z = cosCounter;
 		transform.getRotation().z = counter;
-		transform.getScale().x = cosf(counter);
+		transform.getRotation().y = counter;
+		transform.getRotation().x = counter;
 
 		shader.bind();
 		texture.bind(0);
-		shader.update(transform);
+		shader.update(transform, camera);
 		mesh.draw();
 
 		display.update();
